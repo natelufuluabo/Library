084c33b24fec0586f7fa52079f37225fcce3fc42
Aircongo <natelufuluabo@yahoo.ca>
2022-12-10 08:54:20 -0500

    created customers model

--- models/author.js
+++ models/author.js
@@ -4,8 +4,16 @@ const { DateTime } = require("luxon");
 const Schema = mongoose.Schema;
 
 const AuthorSchema = new Schema({
-  first_name: { type: String, required: true, maxLength: 100 },
-  family_name: { type: String, required: true, maxLength: 100 },
+  first_name: { 
+    type: String, 
+    required: true, 
+    maxLength: 100 
+  },
+  family_name: { 
+    type: String, 
+    required: true, 
+    maxLength: 100 
+  },
   date_of_birth: { type: Date },
   date_of_death: { type: Date },
 });
---
+++ models/users.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const CustomerSchema = new Schema({
+    type : {
+        type : String,
+        enum : ["Customer"],
+        required : true
+    },
+    first_name : {
+        type : String,
+        required : true,
+        maxLength: 100
+    },
+    family_name: { 
+      type: String, 
+      required: true, 
+      maxLength: 100 
+    },
+    date_of_birth: { type: Date },
+    emailAdress : {
+        type : String,
+        required : true
+    },
+    password : {
+        type : String,
+        minLength : 8,
+        maxLength : 16,
+        required : true
+    },
+    bookRented : {
+        type : [Schema.Types.ObjectId],
+        ref : "BookInstance"
+    },
+    favouritesBooks : {
+        type : [Schema.Types.ObjectId],
+        ref : "Book"
+    }
+});
+
+// Export model
+module.exports = mongoose.model("Customer", CustomerSchema);
